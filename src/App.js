c1550833aea22b7ae37041b82cb2991909faa0d4
Tsilos Kosbab <38735446+Tsilos1@users.noreply.github.com>
2022-11-03 21:34:30 -0500

    working on TopSection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import TopBar from './components/TopBar';
+import TopSection from './components/TopSection'
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <TopSection />
       
     </div>
   );
--- src/components/TopSection.js
+++ src/components/TopSection.js
@@ -1,4 +1,5 @@
-import Container from 'react-bootstrap/Container'
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
 
 
 
@@ -6,12 +7,22 @@ export default function TopSection() {
     return(
        <div>
 
-        <card>
-`       `   <card.Img variant="top" src=""
-
-
-
-        </card>
+<Card style={{ width: "100%", height: "428px" }} className="bg-dark text-black">
+            <Card.Img style={{ height: "100%", width: "100%", position: "absolute" }} src="https://www.instacart.com/image-server/x428/www.instacart.com/assets/homepage/homepage_background_full_m3_cropped-8d2d286263821da7decd7c61fb1db1eb0e3dec13e0c356277d6d3cb7484c024a.jpg" alt="Card image" />
+                <Card.ImgOverlay style={{ top: "auto", width: "578px", marginLeft: "100px", marginBottom: "50px"}}>
+                    <Card.Text style={{ fontSize: "2.5em", fontWeight: "bold" }}>
+                        Order groceries for delivery <br /> or pickup today
+                    </Card.Text>
+                    <Card.Text style={{ fontSize: "1em" }}>
+                        Whatever you want from local stores, brought right to your door.
+                    </Card.Text>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Control size="lg" type="text" placeholder="   Enter your address                                                          ->" />
+                        </Form.Group>
+                    </Form>
+                </Card.ImgOverlay>
+            </Card>
 
 
 
